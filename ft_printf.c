0f0e4af9c8f771781d9e4f2decfbabeb6e122865
Jeremy FLEURY <jfleury@e2r1p6.42.fr>
2019-01-03 12:15:08 +0100

    va_arg

--- ft_printf.c
+++ ft_printf.c
@@ -6,7 +6,7 @@
 /*   By: jfleury <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 11:27:25 by jfleury           #+#    #+#             */
-/*   Updated: 2019/01/03 11:59:07 by jfleury          ###   ########.fr       */
+/*   Updated: 2019/01/03 12:14:52 by jfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,15 +15,20 @@
 int		ft_printf(const char *format, ...)
 {
 	va_list		ap;
+	int			d;
+	char		c;
+	char		*s;
 
 	va_start(ap, format);
-
-
-
+	while (*format)
+	{
+		
+	}
 	va_end(ap);
 }
 
 int		main(int ac, char **av)
 {
-
+	printf("%s\n", av[1]);
+	ft_printf("%s\n", av[1]);
 }
