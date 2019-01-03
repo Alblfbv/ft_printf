09abca099fe84e781cde0c8097bcafaa5e1dc72b
Jeremy FLEURY <jfleury@e2r1p6.42.fr>
2019-01-03 16:13:29 +0100

    .5 putnbr putstr

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: jfleury <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/03 11:26:18 by jfleury           #+#    #+#             */
-/*   Updated: 2019/01/03 11:31:01 by jfleury          ###   ########.fr       */
+/*   Updated: 2019/01/03 16:13:15 by jfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,5 +16,8 @@
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg>
+
+void	ft_putstr(char *str);
+void	ft_putnbr(int nb);
 
 #endif
