623d18548afdd4568d8c5bdf762f734df0c5758f
Alban LEFEBVRE <allefebv@student.42.fr>
2019-01-08 10:13:18 +0100

    Poule 4

--- ft_printf.h
+++ ft_printf.h
@@ -13,39 +13,35 @@
 #ifndef FT_PRINTF_H
 # define FT_PRINTF_H
 
-# define CONV_ID_NB 10
-
-typedef struct	s_conv_spec
-{
-	char				conv_id;
-	int					param_value_int;
-	unsigned int		param_value_u_int;
-	double				param_value_double;
-	char 				*param_value_char;
-	void				*param_value_void;
-	
-	int					*flags;
-	int					*size_modif;
-	char				*field_width_base;
-	char				*precision_base;
-
-	int		field_width_final;
-	int		precision_final;
-
-	char*	converted;
-}					t_conv_spec;
-
 # include <stdlib.h>
 # include <unistd.h>
 # include <stdarg.h>
 # include <stdio.h>
 # include "libft/libft.h"
 
+# define CONV_ID_NB 10
+
+typedef struct	s_conv_spec
+{
+	char            conv_id;
+	int             param_value_int;
+	unsigned int	param_value_u_int;
+	double		param_value_double;
+	char 		*param_value_char;
+	void		*param_value_void;	
+	int		*flags;
+	int		*size_modif;
+	char	        *field_width_base;
+	char		*precision_base;
+	int		field_width_final;
+	int		precision_final;
+	char*           converted;
+}					t_conv_spec;
+
 char	*ft_data_conv_ids(void);
 int		ft_conv_id(t_conv_spec *conv_spec, char *format, int i);
 void	ft_size_modif(t_conv_spec *conv_spec, char *format, int i, int len);
 void	ft_param_value(t_conv_spec *conv_spec, va_list *ap);
 void	ft_flag(t_conv_spec *conv_spec, char *format, int i, int len);
-
 
 #endif
