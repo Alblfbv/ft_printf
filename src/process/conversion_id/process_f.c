b04ca636cdc61aecd2a5dfc420b47909042a8bff
Jeremy FLEURY <jfleury@e2r8p2.42.fr>
2019-01-14 19:16:28 +0100

    Pouleeeeeeeeeeeeeeee

--- src/process/conversion_id/process_f.c
+++ src/process/conversion_id/process_f.c
@@ -6,7 +6,7 @@
 /*   By: jfleury <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/14 14:30:04 by jfleury           #+#    #+#             */
-/*   Updated: 2019/01/14 18:35:21 by jfleury          ###   ########.fr       */
+/*   Updated: 2019/01/14 19:15:59 by jfleury          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,10 +15,22 @@
 char	*ft_process_f(t_conv_spec conv_spec, va_list *ap)
 {
 	char	*str;
+	char	*str2;
+	int		tmp;
 	double	nb;
 
-	nb = va_arg(*ap, double);
-	
-	return (str);
+	nb = 12.369;
+	tmp = nb;
+	str = ft_itoa(tmp);
+	str2 = ft_strdup('.');
+	str2 = ft_strextend(str2, str);
+	free(str);
+	nb = nb - tmp;
+	while (conv_spec.precision != 0)
+	{
+		nb = nb * 10;
+
+	}
+	return (str2);
 }
 
