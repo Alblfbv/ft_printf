488feaddbfef401f386a2a0c27dbb5e2718506df
Alban LEFEBVRE <allefebv@student.42.fr>
2019-01-16 18:45:15 +0100

    hash ok + zero corrected

--- src/process/flags/process_zero.c
+++ src/process/flags/process_zero.c
@@ -6,7 +6,7 @@
 /*   By: allefebv <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/01/11 12:16:02 by allefebv          #+#    #+#             */
-/*   Updated: 2019/01/15 12:13:36 by allefebv         ###   ########.fr       */
+/*   Updated: 2019/01/16 18:42:26 by allefebv         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@ char	*ft_process_zero(t_conv_spec conv_spec, char *str)
 {
 	int	i;
 
-	if (conv_spec.precision != -1 || conv_spec.flags[2] == 1
+	if (conv_spec.precision != -1 || conv_spec.flags[4] == 1
 		|| conv_spec.conv_id == 'c' || conv_spec.conv_id == 's'
 		|| conv_spec.conv_id == 'p')
 		return (str);
